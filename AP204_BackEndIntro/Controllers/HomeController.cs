64fefa2b2bb3014cb728b0ec7ada6559af852efb
Ghu1iyev <tu12n4c1r@code.edu.az>
2022-05-23 10:21:46 +0400

    backend

---
+++ AP204_BackEndIntro/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AP204_BackEndIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index( int id)
+        {
+            return Content(id.ToString());
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Product = "Phone";
+            ViewData["Count"]=5;
+            TempData["Price"] = "570 $";
+            return RedirectToAction("Index");
+            return View();
+        }
+        public ActionResult Contact()
+        {
+           
+           
+            return View();
+        }
+        public ActionResult FAQ(string name,string surname)
+        {
+            string fullname = $"{name} ,{surname}";
+            return Content(fullname);
+        }
+    }
+}
